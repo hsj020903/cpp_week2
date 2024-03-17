3f12aba48c6c826834e1637dff776300fce564e3
hsj020903 <hsj020903@naver.com>
2024-03-17 18:45:27 +0900

    fix: 정답 숫자가 출력되지 않도록 변경

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@ void util(int number[]);
 int main(){
     int number[3];
     util(number);
-    cout << "Answer is " << number[0] << number[1] << number[2] << '\n' ;
     game(number);
 
     cout << "You win!";
